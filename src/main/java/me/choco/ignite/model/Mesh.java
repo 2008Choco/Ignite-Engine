e5bde922abd7d5dacd12e01f2bb7fc8424d60913
Parker Hawke <hawkeboyz2@hotmail.com>
2018-05-24 09:52:24 -0400

    Add Mesh#getVerticesAsBuffer() and Mesh#getIndicesAsBuffer() methods
    Will be used for buffering data to VBOs

--- src/main/java/me/choco/ignite/model/Mesh.java
+++ src/main/java/me/choco/ignite/model/Mesh.java
@@ -1,6 +1,12 @@
 package me.choco.ignite.model;
 
+import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
 import java.util.Arrays;
+
+import org.joml.Vector2f;
+import org.joml.Vector3f;
+import org.lwjgl.system.MemoryUtil;
 
 public class Mesh {
 	
@@ -26,6 +32,42 @@ public class Mesh {
 	
 	public int[] getIndices() {
 		return indices;
+	}
+	
+	public FloatBuffer getVerticesAsBuffer(boolean flip) {
+		FloatBuffer buffer = MemoryUtil.memAllocFloat(vertices.length * Vertex.BYTES);
+		
+		for (Vertex vertex : vertices) {
+			Vector3f position = vertex.getPosition();
+			buffer.put(position.x);
+			buffer.put(position.y);
+			buffer.put(position.z);
+			
+			Vector3f normal = vertex.getNormal();
+			buffer.put(normal.x);
+			buffer.put(normal.y);
+			buffer.put(normal.z);
+			
+			Vector2f textureCoordinate = vertex.getTextureCoordinates();
+			buffer.put(textureCoordinate.x);
+			buffer.put(textureCoordinate.y);
+		}
+		
+		if (flip) {
+			buffer.flip();
+		}
+		
+		return buffer;
+	}
+	
+	public IntBuffer getIndicesAsBuffer(boolean flip) {
+		IntBuffer buffer = MemoryUtil.memAllocInt(indices.length).put(indices);
+		
+		if (flip) {
+			buffer.flip();
+		}
+		
+		return buffer;
 	}
 
 	@Override
