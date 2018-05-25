2e4a67044cf2111a975a821a671447982e4b6086
Parker Hawke <hawkeboyz2@hotmail.com>
2018-05-25 10:09:27 -0400

    Add a Transformation class to handle transformations. Add field to Model

--- src/main/java/me/choco/ignite/model/Model.java
+++ src/main/java/me/choco/ignite/model/Model.java
@@ -2,25 +2,36 @@ package me.choco.ignite.model;
 
 import java.util.Objects;
 
+import com.google.common.base.Preconditions;
+
+import me.choco.ignite.world.Transformation;
+
 public class Model {
 	
-	private Mesh mesh;
+	private final Mesh mesh;
+	private final Transformation transformation;
 	
 	public Model(Mesh mesh) {
+		Preconditions.checkNotNull(mesh, "mesh");
+		
 		this.mesh = mesh;
-	}
-	
-	public void setMesh(Mesh mesh) {
-		this.mesh = mesh;
+		this.transformation = new Transformation();
 	}
 	
 	public Mesh getMesh() {
 		return mesh;
 	}
 	
+	public Transformation getTransformation() {
+		return transformation;
+	}
+	
 	@Override
 	public int hashCode() {
-		return 31 * mesh.hashCode();
+		int result = 31 * (mesh == null ? 0 : mesh.hashCode());
+		result = 31 * result + (transformation == null ? 0 : transformation.hashCode());
+		
+		return result;
 	}
 	
 	@Override
@@ -29,7 +40,7 @@ public class Model {
 		if (!(obj instanceof Model)) return false;
 		
 		Model other = (Model) obj;
-		return Objects.equals(mesh, other.mesh);
+		return Objects.equals(mesh, other.mesh) && Objects.equals(transformation, other.transformation);
 	}
 	
 }
